5f1d3b6cef3f25a7bfe6740401013f85f8c26bdf
Artem <chief.marik2014@yandex.ru>
2022-04-11 13:56:48 +0300

    added AddEvent

--- Coursera White Belt.cpp
+++ Coursera White Belt.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <exception>
 #include <string>
+#include <set>
+#include <map>
 
 using namespace std;
 
@@ -36,13 +38,38 @@ bool operator<(const Date& lhs, const Date& rhs) {
 
 class Database {
 public:
-    void AddEvent(const Date& date, const string& event);
-    bool DeleteEvent(const Date& date, const string& event);
-    int  DeleteDate(const Date& date);
+    void AddEvent(const Date& date, const string& event) {
+        new_date.insert(date);
+        date_event[event] = new_date;
+    }
+
+    bool DeleteEvent(const Date& date, const string& event) {
+        cin >> operation_word;
+
+        if (date_event[operation_word] == date_event[event]) {
+            date_event.erase(event);
+            cout << "Deleted successfully" << endl;
+            return true;
+        }
+        else {
+            cout <<  "Event not found" << endl;
+            return false;
+        }
+    }
+
+    int  DeleteDate(const Date& date) {
+        cin >> date;
+    }
 
     /* ??? */ Find(const Date& date) const;
 
     void Print() const;
+
+private:
+    map <string, set <Date>> date_event;
+    set <Date> new_date;
+    string operation_word;
+    set <Date> date_for_del;
 };
 
 int main() {
